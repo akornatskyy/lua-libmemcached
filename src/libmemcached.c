02b86318d679175f4b9d8f6fb570f46b6d9f1ee5
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2015-10-10 09:57:25 +0300

    Disabled touch operation.

--- src/libmemcached.c
+++ src/libmemcached.c
@@ -450,7 +450,7 @@ l_delete(lua_State *L)
     return l_error(L, rc);
 }
 
-
+/*
 static int
 l_touch(lua_State *L)
 {
@@ -477,7 +477,7 @@ l_touch(lua_State *L)
 
     return l_error(L, rc);
 }
-
+*/
 
 static int
 l_incr_decr(lua_State *L, memcached_incr_pt f)
@@ -621,7 +621,7 @@ luaopen_libmemcached(lua_State *L)
         { "append", l_append },
         { "prepend", l_prepend },
         { "delete", l_delete },
-        { "touch", l_touch },
+        // { "touch", l_touch },
         { "incr", l_incr },
         { "decr", l_decr },
         // { "exist", l_exist },
